0c4b40c9f79d2ab215cd269d508e98bae2d6eed2
invalidusernamecalled <puneetbapna@gmail.com>
2025-01-28 16:04:34 +0530

    Delete customcommand.bat

--- customcommand.bat
+++
@@ -1,9 +0,0 @@
-color 09
-doskey del=del /p $1
-doskey up=cd ..\
-doskey cd="C:\Users\ANILBAPNA\AppData\Local\Temp\cd.bat" $1
-doskey 00=cd \
-doskey down="C:\Users\ANILBAPNA\AppData\Local\Temp\down.bat"
-doskey clear=del "C:\Users\ANILBAPNA\AppData\Local\Temp\kmxlkamsalkjasa9019820.txt"
-doskey whris=dir /s /b "$*"
-cls
